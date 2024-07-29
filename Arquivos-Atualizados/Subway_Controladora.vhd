98695822643415b6548032d1007afb8518e4a4b9
Ohana-Souza <84847503+Ohana-Souza@users.noreply.github.com>
2024-07-29 15:32:33 -0300

    Update Subway_Controladora.vhd

--- Arquivos-Atualizados/Subway_Controladora.vhd
+++ Arquivos-Atualizados/Subway_Controladora.vhd
@@ -20,12 +20,9 @@ entity Subway_Controladora is
         DR_PortaAberta    			: out STD_LOGIC;                -- LED vermelho
         DR_Tecnico   			   : out STD_LOGIC;                -- LED vermelho indicando que o técnico foi chamado
 		  DR_CHEGOU						: out STD_LOGIC; 					  -- Led verde indicando a chegada da cápsula
-        DR_Emerg_SigOUT				: out STD_LOGIC; 					  -- Led vermelho indicando o acionamento da emergência
+        DR_Emerg_SigOUT				: out STD_LOGIC					  -- Led vermelho indicando o acionamento da emergência
 		 
-		  lcd_rs             : out STD_LOGIC;                      -- LCD Register Select
-		  lcd_rw             : out STD_LOGIC;                      -- LCD Read/Write
-		  lcd_e              : out STD_LOGIC;                      -- LCD Enable
-		  lcd_data           : out STD_LOGIC_VECTOR(7 downto 0)    -- LCD Data Bus
+		
     );
 end entity Subway_Controladora;
 
@@ -33,7 +30,7 @@ architecture Behavioral of Subway_Controladora is
 
     type state_type is (IDLE, WAITING, SENDING, ARRIVED, EMERGENCY);
     signal state : state_type := IDLE;
-    signal lcd_message : string(1 to 16) := "                "; -- Mensagem de 16 caracteres para o LCD
+  
     constant LIBERATION_CODE  : std_logic_vector(2 downto 0) := "011"; -- Substitua por código real
     
 
@@ -41,7 +38,7 @@ begin
 
  
     -- Processo de controle principal
-    process (clk, DR_Reset)
+    process (clk, DR_Reset, DR_Emer_Solucionado)
     begin
         if (DR_Reset = '1') or (DR_Emer_Solucionado = '1') then
             state <= IDLE;
@@ -50,7 +47,7 @@ begin
             DR_Tecnico <= '0';
             DR_CHEGOU <= '0';
             DR_Emerg_SigOUT <= '0';
-            lcd_message <= "BEM VINDO!      ";
+          
         elsif rising_edge(clk) then
             case state is
                 when IDLE =>
@@ -58,11 +55,11 @@ begin
                         state <= WAITING;
                         DR_PortaFechada <= '1';
                         DR_PortaAberta <= '0';
-                        lcd_message <= "DADOS INSERIDOS ";
+                        
                     elsif DR_ENVIAR = '1' and DR_Porta = '0' then
                         DR_PortaFechada <= '0';
                         DR_PortaAberta <= '1';
-                        lcd_message <= "PORTA ABERTA    ";
+                       
                     end if;
 
                 when WAITING =>
@@ -75,7 +72,7 @@ begin
                         state <= SENDING;
                         DR_PortaFechada <= '1';
                         DR_PortaAberta <= '0';
-                        lcd_message <= "ENVIANDO        ";
+                        
                     end if;
 
                 when SENDING =>
@@ -84,13 +81,13 @@ begin
                         DR_PortaFechada <= '0';
                         DR_PortaAberta <= '0';
                         DR_Tecnico <= '0';
-                        lcd_message <= "INICIO          ";
+                       
                     elsif DR_CapsuleArrived = '1' then
                         state <= ARRIVED;
                         DR_PortaFechada <= '1';
                         DR_PortaAberta <= '0';
                         DR_CHEGOU <= '1';
-                        lcd_message <= "CHEGOU          ";
+                       
                     end if;
 
                 when ARRIVED =>
@@ -100,52 +97,24 @@ begin
                         DR_PortaAberta <= '0';
                         DR_Tecnico <= '0';
                         DR_CHEGOU <= '0';
-                        lcd_message <= "LIBERADO        ";
+                       
                     end if;
 
                 when EMERGENCY =>
                     DR_Emerg_SigOUT <= '1';
                     DR_Tecnico <= '1';
-                    lcd_message <= "EMERGENCIA      ";
+                 
                     if DR_Emer_Solucionado = '1' then
                         state <= IDLE;
                         DR_Emerg_SigOUT <= '0';
                         DR_Tecnico <= '0';
-                        lcd_message <= "  SOLUCIONADO   ";
+                      
                     end if;
 
                 when others =>
                     state <= IDLE;
 
             end case;
-        end if;
-    end process;
-
-    -- Processo para atualizar o LCD
-    process (clk)
-    variable counter : natural := 0;
-    begin
-        if rising_edge(clk) then
-            -- Lógica para enviar a mensagem lcd_message ao display LCD
-            lcd_rs <= '0';
-            lcd_rw <= '0';
-            lcd_e <= '1';
-            lcd_data <= "00001100"; -- Comando para ligar o display sem cursor
-
-            for i in 1 to 16 loop
-                lcd_rs <= '1';
-                lcd_rw <= '0';
-                lcd_e <= '1';
-                lcd_data <= std_logic_vector(to_unsigned(character'pos(lcd_message(i)), 8));
-
-                -- Adicione um pequeno atraso (por exemplo, 10 ciclos de clock)
-                if counter < 10 then
-                    counter := counter + 1;
-                else
-                    counter := 0; -- Reinicia o contador
-                    lcd_e <= '0'; -- Desativa o sinal de enable após o envio dos dados
-                end if;
-            end loop;
         end if;
     end process;
 
